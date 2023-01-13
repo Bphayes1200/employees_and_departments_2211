a9fcf7da0c48f19167979417a598c38ce1f4677a
Brian Hayes <Brian.hayes1200@gmail.com>
2023-01-13 09:14:11 -0700

    Test: add test for give raise method for class Employee

--- spec/employee_spec.rb
+++ spec/employee_spec.rb
@@ -9,4 +9,14 @@ RSpec.describe Employee do
     expect(bobbi.age).to eq(30)
     expect(bobbi.salary).to eq(100000)
   end
+
+  it 'will give a raise to an employee' do 
+    bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"})
+
+    expect(bobbi.salary).to eq(100000)
+
+    bobbi.give_raise(5000)
+    
+    expect(bobbi.salary).to eq(10500)
+  end
 end
