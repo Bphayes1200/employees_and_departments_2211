8823c3c9e5d498302b22cf6e2b07384d4ba3b6ee
Brian Hayes <Brian.hayes1200@gmail.com>
2023-01-13 09:37:47 -0700

    Test: add test for add department method for class budget

--- spec/budget_spec.rb
+++ spec/budget_spec.rb
@@ -10,4 +10,20 @@ RSpec.describe Budget do
     expect(budget.year).to eq("2023")
     expect(budget.departments).to eq([])
   end
+
+  it 'can add departements' do 
+    budget = Budget.new("2023")
+    customer_service = Department.new("Customer Service")
+    sales = Department.new("Sales")
+    bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "100000"})
+    aaron = Employee.new({name: "Aaron Tanaka", age: "25", salary: "90000"}) 
+    brian = Employee.new({name: "Brian Hayes", age: "20", salary: "1000000000"})
+    customer_service.hire(bobbi)
+    customer_service.hire(aaron)  
+    sales.hire(brian)
+    budget.add_department(customer_service)
+    budget.add_department(sales)
+
+    expect(budget.departments).to eq([customer_service, sales])
+  end
 end
