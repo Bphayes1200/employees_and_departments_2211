c0177cdb7466adc3ec2a9d43a2d810735c657a08
Brian Hayes <Brian.hayes1200@gmail.com>
2023-01-13 09:48:23 -0700

    Test: add test for employee_salaries method for class Budget

--- spec/budget_spec.rb
+++ spec/budget_spec.rb
@@ -46,4 +46,24 @@ RSpec.describe Budget do
 
     expect(budget.low_expenses).to eq([customer_service])
   end
+
+  it 'will list all of the employees salaries' do 
+    budget = Budget.new("2023")
+    customer_service = Department.new("Customer Service")
+    sales = Department.new("Sales")
+    bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "100000"})
+    aaron = Employee.new({name: "Aaron Tanaka", age: "25", salary: "90000"}) 
+    brian = Employee.new({name: "Brian Hayes", age: "20", salary: "1000000000"})
+    customer_service.hire(bobbi)
+    customer_service.hire(aaron)
+    sales.hire(brian)
+    budget.add_department(customer_service)
+    budget.add_department(sales)
+    
+    expect(budget.emloyee_salaries).to eq({
+      "Bobbi Jaeger" => 100000, 
+      "Aaron Tanaka" => 90000, 
+      "Brian Hayes" =>  1000000000 
+    })
+  end
 end
