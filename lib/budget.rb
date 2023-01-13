2c2c33bb392bf7c183099e2f02b76aad8b795ec4
Brian Hayes <Brian.hayes1200@gmail.com>
2023-01-13 09:44:52 -0700

    Feat: add low_expenses method for class Budget

--- lib/budget.rb
+++ lib/budget.rb
@@ -9,4 +9,8 @@ class Budget
   def add_department(department)
     @departments << department
   end
+
+  def low_expenses
+    @departments.find_all { |department| department.expenses < 500}
+  end
 end
